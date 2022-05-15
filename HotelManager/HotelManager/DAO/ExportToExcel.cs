3b001b4b17ca175799d5134aa8069fc2ffad9506
Hoang Tran <hnbtranspkt@gmail.com>
2022-05-15 21:29:34 +0700

    [Initialize] upload source code and sql

---
+++ HotelManager/HotelManager/DAO/ExportToExcel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System;
+
+namespace HotelManager.DAO
+{
+    public enum ModeExportToExcel
+    {
+        XLS = 0,
+        XLSX = 1,
+        PDF = 2
+    }
+    public class ExportToExcel
+    {
+        private static ExportToExcel instance;
+        public static ExportToExcel Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ExportToExcel();
+                return instance;
+            }
+        }
+        private ExportToExcel() { }
+        public bool Export(DataGridView dataGridView, string path, ModeExportToExcel mode = ModeExportToExcel.XLS)
+        {
+            return false;
+        }
+
+        private void ToExcel()
+        {
+        }
+    }
+}
